9fcba73fa4dd6c38ac105afcbf8702d7104e5ba0
Arturo Crespo de la Viña <acrespo@full-on-net.com>
2021-03-23 12:26:51 +0100

    Changes in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-echo ${INPUT_CI_USER_NAME}
-echo ${INPUT_CI_TOKEN}
-echo ${INPUT_CONTINT_TOKEN_ART}
-echo ${INPUT_ARTIFACTORY_URL}
+echo "Hello!!"
+echo "INPUT_CI_USER_NAME: ${INPUT_CI_USER_NAME}"
+echo "INPUT_CI_TOKEN: ${INPUT_CI_TOKEN}"
+echo "INPUT_CONTINT_TOKEN_ART: ${INPUT_CONTINT_TOKEN_ART}"
+echo "INPUT_ARTIFACTORY_URL: ${INPUT_ARTIFACTORY_URL}"
 
-output=$( bash -c "pwd")
+git clone https://$INPUT_CI_USER_NAME:$INPUT_CI_TOKEN@github.com/Telefonica/$INPUT_TLF_BFW.git
+output=$( python3 $INPUT_TLF_BFW/tlfbuild.py --help )
+
 echo "::set-output name=output_message::$output"
 echo $output
