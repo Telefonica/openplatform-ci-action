a030575f3d685cc2349f9333e4ac6ae95f515602
Arturo Crespo de la Viña <acrespo@full-on-net.com>
2021-03-23 12:34:33 +0100

    Fixing entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "INPUT_CI_TOKEN: ${INPUT_CI_TOKEN}"
 echo "INPUT_CONTINT_TOKEN_ART: ${INPUT_CONTINT_TOKEN_ART}"
 echo "INPUT_ARTIFACTORY_URL: ${INPUT_ARTIFACTORY_URL}"
 
-git clone https://$INPUT_CI_USER_NAME:$INPUT_CI_TOKEN@github.com/Telefonica/$INPUT_TLF_BFW.git
+git clone https://$INPUT_CI_USER:$INPUT_CI_TOKEN@github.com/Telefonica/$INPUT_TLF_BFW.git
 output=$( python3 $INPUT_TLF_BFW/tlfbuild.py --help )
 
 echo "::set-output name=output_message::$output"
