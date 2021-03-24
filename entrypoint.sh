3647f8ec418db0c38c6687ca4ea24d14e76d37a3
Arturo Crespo de la Viña <acrespo@full-on-net.com>
2021-03-24 13:58:49 +0100

    Adding printenv

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,7 @@
 set -e
 
 echo "Hello!!"
-echo "INPUT_CI_USER: ${INPUT_CI_USER}"
-echo "INPUT_CI_TOKEN: ${INPUT_CI_TOKEN}"
-echo "INPUT_CONTINT_TOKEN_ART: ${INPUT_CONTINT_TOKEN_ART}"
-echo "INPUT_ARTIFACTORY_URL: ${INPUT_ARTIFACTORY_URL}"
+printenv
 
 git clone https://$INPUT_CI_USER:$INPUT_CI_TOKEN@github.com/Telefonica/$INPUT_TLF_BFW.git
 output=$( python3 $INPUT_TLF_BFW/tlfbuild.py --help )
